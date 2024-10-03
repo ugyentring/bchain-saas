c07d573a7ffde7bbeaec8477d46f644c74d98c78
ugyen.dev <dev.ugyen@gmail.com>
2024-10-03 18:12:37 +0600

    done with constants and other utility files

---
+++ Context/constants.js
@@ -0,0 +1,11 @@
+import ERC20Generator from "./ERC20Generator.json";
+import LookUpContract from "./LookUpContract.json";
+
+export const ERC20Generator_ABI = ERC20Generator.abi;
+export const ERC20Generator_BYTECODE = ERC20Generator.bytecode;
+export const ERC20Generator_Address =
+  "0xcec8EDdaA91Fb6c422DeCf7d60c305551A26af71";
+
+export const LookUpContract_ABI = LookUpContract.abi;
+export const LookUpContract_Address =
+  "0xd82D4bDdd6C582326EDd2FF663657dCd5bf56cC4";
---
+++ Utils/index.js
@@ -0,0 +1,93 @@
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+import {
+  LookUpContarct_ABI,
+  LookUpContarct_ADDRESS,
+  ERC20Generator_ADDRESS,
+  ERC20Generator_ABI,
+} from "../Context/constants";
+
+export const CheckIfWalletIsConnected = async () => {
+  try {
+    if (!window.ethereum) return alert("Please install metamask");
+
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
+
+    const firstAccount = accounts[0];
+    return firstAccount;
+  } catch (error) {
+    console.log(error);
+    throw new Error("No ethereum object");
+  }
+};
+
+export const ConnectWallet = async () => {
+  try {
+    if (!window.ethereum) return alert("Please install metamask");
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    const firstAccount = accounts[0];
+    return firstAccount;
+  } catch (error) {
+    console.log(error);
+    throw new Error("No ethereum object");
+  }
+};
+
+const fetchContract = (signerOrProvider) => {
+  new ethers.Contract(
+    LookUpContarct_ADDRESS,
+    LookUpContarct_ABI,
+    signerOrProvider
+  );
+};
+
+export const connectingWithContract = async () => {
+  try {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = fetchContract(signer);
+    return contract;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getBalance = async () => {
+  try {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    return await signer.getBalance();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const fetchTokenContract = (signerOrProvider) => {
+  new ethers.Contract(
+    ERC20Generator_ADDRESS,
+    ERC20Generator_ABI,
+    signerOrProvider
+  );
+};
+
+export const connectingNativeTokenContract = async () => {
+  try {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = fetchTokenContract(signer);
+    return contract;
+  } catch (error) {
+    console.log(error);
+  }
+};

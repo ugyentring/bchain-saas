8cb8a2180d97aca77f1b4025dddf3904272212fb
ugyen.dev <dev.ugyen@gmail.com>
2024-10-03 17:44:02 +0600

    deploy contract to sepolia network

---
+++ readme.md
@@ -0,0 +1,2 @@
+ERC20Generator deployed to: 0xcec8EDdaA91Fb6c422DeCf7d60c305551A26af71
+LookUpContract deployed to: 0xd82D4bDdd6C582326EDd2FF663657dCd5bf56cC4
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,25 @@
 const hre = require("hardhat");
+const { ethers } = hre;
 
-//convert to ether
 const tokens = (_number) => {
   return ethers.utils.parseUnits(_number.toString(), "ether");
 };
 
 async function main() {
-  //ERC20 Generator
   const _tokenName = "UT";
   const _tokenSymbol = "UT";
   const _initialSupply = tokens(1000);
 
   const ERC20Generator = await hre.ethers.getContractFactory("ERC20Generator");
   const erc20Generator = await ERC20Generator.deploy(
+    _initialSupply,
     _tokenName,
-    _tokenSymbol,
-    _initialSupply
+    _tokenSymbol
   );
 
   await erc20Generator.deployed();
   console.log("ERC20Generator deployed to:", erc20Generator.address);
 
-  //lookup contract
   const LookUpContract = await hre.ethers.getContractFactory("LookUpContract");
   const lookUpContract = await LookUpContract.deploy();
 

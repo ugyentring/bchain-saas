b91fc79895347d712642f38022359b4f5be2ed10
ugyen.dev <dev.ugyen@gmail.com>
2024-10-10 18:45:00 +0600

    header and create page done

--- pages/create.js
+++ pages/create.js
@@ -30,4 +30,41 @@ const create = () => {
     transferNativeToken,
     transferERC20,
   } = useStateContext();
+
+  return (
+    <div>
+      <Header />
+      {/* {active && <ERC20 setActive={setActive} createERC20={createERC20} />}
+      {transfer && (
+        <Transfer
+          setTransfer={setTransfer}
+          transferNativeToken={transferNativeToken}
+        />
+      )} */}
+
+      <main>
+        <UserProfile />
+        {/* <Profile
+          getAllERC20TokensListed={getAllERC20TokensListed}
+          getAllERC20Tokens={getAllERC20Tokens}
+          getAllDonations={getAllDonations}
+          balance={balance}
+          fee={fee}
+          transferNativeToken={transferNativeToken}
+          mainBalance={mainBalance}
+          nativeToken={nativeToken}
+          address={address}
+          getUserERC20Tokens={getUserERC20Tokens}
+          widthdrawFund={widthdrawFund}
+          donationFund={donationFund}
+          transferERC20={transferERC20}
+          setActive={setActive}
+          setTransfer={setTransfer}
+        /> */}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
 };
+
+export default create;

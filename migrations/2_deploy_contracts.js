833b86a77f922a2b83b19021f8a080da9b752902
ugyentring <tsheringugyen483@gmail.com>
2024-11-07 03:30:27 +0600

    changes in deployment scripts

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,18 @@
+const ERC20Generator = artifacts.require("ERC20Generator");
+const LookUpContract = artifacts.require("LookUpContract");
+
+module.exports = async function (deployer) {
+  const tokenName = "UGYEN";
+  const tokenSymbol = "UT";
+  const initialSupply = web3.utils.toWei("1000", "ether");
+
+  // Deploy ERC20Generator
+  await deployer.deploy(ERC20Generator, initialSupply, tokenName, tokenSymbol);
+  const erc20Generator = await ERC20Generator.deployed();
+  console.log("ERC20Generator deployed to:", erc20Generator.address);
+
+  // Deploy LookUpContract
+  await deployer.deploy(LookUpContract);
+  const lookUpContract = await LookUpContract.deployed();
+  console.log("LookUpContract deployed to:", lookUpContract.address);
+};

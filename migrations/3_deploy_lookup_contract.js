da9131ba467948407bc7df2eb67bd14e89735940
ugyentring <tsheringugyen483@gmail.com>
2024-11-07 03:31:21 +0600

    chnages

--- migrations/3_deploy_lookup_contract.js
+++
@@ -1,8 +0,0 @@
-const LookUpContract = artifacts.require("LookUpContract");
-
-module.exports = async function (deployer) {
-  console.log("Deploying LookUpContract...");
-  await deployer.deploy(LookUpContract);
-  const lookUpContract = await LookUpContract.deployed();
-  console.log("LookUpContract deployed to:", lookUpContract.address);
-};

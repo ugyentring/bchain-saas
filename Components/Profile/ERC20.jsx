358b2e90f812798a5f196a777749d77c98eebe31
ugyentring <tsheringugyen483@gmail.com>
2024-11-08 09:44:32 +0600

    changes

--- Components/Profile/ERC20.jsx
+++ Components/Profile/ERC20.jsx
@@ -18,84 +18,61 @@ const ERC20 = ({ setActive, createERC20 }) => {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-xl-6 col-lg-6 col-md-8">
-            <div className="login-form signup-form p-4 shadow rounded">
+            <div className="login-form signup-form">
               <span
                 onClick={() => setActive(false)}
-                className="close-btn"
-                style={{
-                  display: "inline-block",
-                  padding: "8px",
-                  cursor: "pointer",
-                  fontSize: "20px",
-                  color: "#333",
-                }}
               >
                 <AiOutlineClose />
               </span>
-              <h4 className="login-title text-center mb-4">Create ERC20 Token</h4>
+              <h4 className="login-title text-center">Create ERC20 Token</h4>
 
-              <div id="contactForm" className="login-form">
+              <div id="contactForm" className="log-form">
                 <input
                   type="text"
                   id="name"
-                  className="form-control mb-3 p-2"
-                  placeholder="Token Name"
+                  className="form-control"
+                  placeholder="Name"
                   required
                   onChange={(e) => handleTokenInfo("name", e)}
-                  style={{
-                    border: "1px solid #ddd",
-                    borderRadius: "4px",
-                  }}
                 />
 
                 <input
                   type="text"
                   id="symbol"
-                  className="form-control mb-3 p-2"
-                  placeholder="Token Symbol"
+                  className="form-control"
+                  placeholder="Symbol"
                   required
                   onChange={(e) => handleTokenInfo("symbol", e)}
-                  style={{
-                    border: "1px solid #ddd",
-                    borderRadius: "4px",
-                  }}
                 />
 
                 <input
                   type="number"
-                  id="supply"
-                  className="form-control mb-3 p-2"
+                  id="msg_subject"
+                  className="form-control"
                   placeholder="Total Supply"
                   required
                   onChange={(e) => handleTokenInfo("supply", e)}
-                  style={{
-                    border: "1px solid #ddd",
-                    borderRadius: "4px",
-                  }}
                 />
 
                 <button
                   onClick={() => createERC20(token)}
                   id="submit"
                   type="submit"
-                  className="btn btn-primary w-100 py-2 mt-3"
-                  style={{
-                    borderRadius: "4px",
-                    backgroundColor: "#007bff",
-                    color: "#fff",
-                  }}
+                  className="slide-btn color-btn login-btn"
                 >
                   Create Token
                 </button>
 
-                <div id="msgSubmit" className="h3 text-center hidden mt-3"></div>
-                <div className="separator text-center mt-4">
+                <div id="msgSubmit" className="h3 text-center hidden"></div>
+                <div className="clearfix"></div>
+                <div className="clear"></div>
+                <div className="separator text-center">
                   <span>Create Your ERC20 Token</span>
                 </div>
 
-                <div className="sign-icon text-center mt-2">
+                <div className="sign-icon">
                   <div className="acc-not">
-                    with a minimum fee of <a href="#">0.01 Sepolia</a>
+                    with a minimum fee of <a>1 Sepolia</a>
                   </div>
                 </div>
               </div>
